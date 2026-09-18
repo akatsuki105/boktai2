a84b36b2bc2388d45adfae10b2dd78c5a45e82e1
akatsuki105 <bluejapan73+dev@gmail.com>
2026-09-19 05:53:12 +0900

    wip

--- include/struct.h
+++ include/struct.h
@@ -10,21 +10,21 @@ struct MainSprite;
 
 // Collision or 座標計算 or スクリプト生成管理 のデータ?(まだわからない)
 typedef struct Entity2UnkData {
-  u16 id;     // 0x00, EntityのID?
-  u16 unk_2;  // 0x02, フラグっぽい
-  u8 unk_4;   // 0x04
-  u8 unk_5;   // 0x05
-  u8 unk_6[2];
-  Vec3 pos;                   // 0x08
-  Vec3 delta;                 // 0x10, FUN_0823b4b8 で .pos の変化量として使われている
-  void* unk_18;               // 0x18
-  u16 unk_1c;                 // 0x1C
-  u16 unk_1e;                 // 0x1E
-  u32 unk_20;                 // 0x20
-  void* unk_24;               // 0x24
-  u32 unk_28;                 // 0x28
-  struct MainSprite* unk_2c;  // 0x2C
-  u8 unk_30[8];
+  u16 id;                       // 0x00, EntityのID?
+  u16 unk_2;                    // 0x02, フラグっぽい
+  u8 unk_4;                     // 0x04
+  u8 unk_5;                     // 0x05
+  u8 unk_6[2];                  // 0x06, 読み手も書き手も未発見
+  Vec3 pos;                     // 0x08
+  Vec3 delta;                   // 0x10, FUN_0823b4b8 で .pos の変化量として使われている
+  void* unk_18;                 // 0x18
+  u16 unk_1c;                   // 0x1C
+  u16 unk_1e;                   // 0x1E
+  u32 unk_20;                   // 0x20
+  void* unk_24;                 // 0x24
+  u32 unk_28;                   // 0x28
+  struct MainSprite* unk_2c;    // 0x2C
+  Vec3 unk_30;                  // 0x30, FUN_0823b47c が引数から8バイトまとめて書き、unk_2 に bit2 を立てる。読み手は未発見
   void* p_38;                   // 0x38, 親構造体のポインタ?
   struct Entity2UnkData* prev;  // 0x3C
   struct Entity2UnkData* next;  // 0x40
