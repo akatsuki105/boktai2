055a12de14b42c5ee9005e1cc182a1cd76fc0238
akatsuki105 <bluejapan73+dev@gmail.com>
2026-09-26 04:14:47 +0900

    EnemyDexMenu

--- src/enemy_dex.c
+++ src/enemy_dex.c
@@ -1,9 +1,49 @@
 #include "entity.h"
 #include "global.h"
 
+struct TextPanel;
+struct EntityC946;
+
+// _Init がスクリプトキーワード 't' から4バイトずつ57件読む
 typedef struct {
-  Entity e;  // 0x0, ENTITY_UNK_8
-  u8 unk_18[4704 - sizeof(Entity)];
+  u8 unk_0;  // 0x00
+  u8 unk_1;  // 0x01
+  u8 unk_2;  // 0x02
+  u8 unk_3;  // 0x03
+} EnemyDexEntry;
+static_assert(sizeof(EnemyDexEntry) == 4);
+
+// 敵図鑑のメニュー。スクリプトのキーワードから表示内容を組み立てる
+typedef struct {
+  Entity e;                        // 0x0000, ENTITY_UNK_8
+  u8 unk_18[8];                    // 0x0018
+  u32 unk_20;                      // 0x0020, _Init が 0
+  u8 unk_24;                       // 0x0024, _Init が 0
+  u8 unk_25;                       // 0x0025, _Init が 0
+  u8 unk_26;                       // 0x0026, _Init が 0
+  u8 unk_27;                       // 0x0027
+  struct TextPanel* panels[8];     // 0x0028, _Init が TextPanel_Create を8回まわして詰める
+  u8 unk_48;                       // 0x0048, _Init が 0
+  u8 unk_49;                       // 0x0049, _Init が 0x20
+  u8 unk_4a;                       // 0x004A, _Init が 0
+  u8 unk_4b[0x110C - 0x4B];        // 0x004B
+  u16 unk_110c;                    // 0x110C, _Init が 0
+  u16 unk_110e;                    // 0x110E, _Init が 5
+  void* scriptN;                   // 0x1110, キーワード 'n'。TextPanel_SetScript に渡す
+  u8 unk_1114[57];                 // 0x1114, キーワード 'd' の Script_GetValue を57バイト
+  u8 unk_114d[3];                  // 0x114D
+  EnemyDexEntry entries[57];       // 0x1150, キーワード 't'
+  void* scriptI;                   // 0x1234, キーワード 'i'
+  void* scriptP;                   // 0x1238, キーワード 'p'
+  void* scriptS;                   // 0x123C, キーワード 's'
+  void* scriptW;                   // 0x1240, キーワード 'w'
+  void* scriptA_upper;             // 0x1244, キーワード 'A'
+  void* scriptA;                   // 0x1248, キーワード 'a'
+  void* scriptM;                   // 0x124C, キーワード 'm'
+  void* scriptU;                   // 0x1250, キーワード 'u'
+  void* scriptT_upper;             // 0x1254, キーワード 'T'
+  u32 unk_1258;                    // 0x1258, VM_GetKeywordValue('e', 0)
+  struct EntityC946* sub;          // 0x125C, EntityC946_Create(0, 0) の戻り値
 } EnemyDexMenu;
 static_assert(sizeof(EnemyDexMenu) == 4704);
 
