ab7aefb2dec539de903129e0cae4246220c44323
akatsuki105 <bluejapan73+dev@gmail.com>
2026-09-26 04:28:52 +0900

    Entity080ac574

--- src/entity_080ac574.c
+++ src/entity_080ac574.c
@@ -1,9 +1,21 @@
 #include "entity.h"
+#include "particle.h"
 #include "global.h"
 
+// 4個を使い回す粒子の枠
 typedef struct {
-  Entity e;  // ENTITY_UNK_9
-  u8 unk_18[280 - 0x18];
+  Particle ptcl;        // 0x00, _Destroy が Particle_Remove に渡す
+  u8 unk_28[8];         // 0x28
+  ParticleGroup* group; // 0x30, _Init が GetParticleGroup(PTCL_GROUP_2) を入れる
+  u8 unk_34[2];         // 0x34
+  u16 active;           // 0x36, _Update が 0 の枠を飛ばす
+  u8 unk_38[8];         // 0x38
+} Entity080ac574Particle;
+static_assert(sizeof(Entity080ac574Particle) == 64);
+
+typedef struct {
+  Entity e;                         // 0x00, ENTITY_UNK_11
+  Entity080ac574Particle ptcls[4];  // 0x18, 根拠: _Destroy / _Update / _Init の stride 0x40 × 4
 } Entity080ac574;
 static_assert(sizeof(Entity080ac574) == 280);
 
