b270164b4a9826716491dbcd6359824c28bbd25d
akatsuki105 <bluejapan73+dev@gmail.com>
2026-09-26 06:15:49 +0900

    Player_WeaponEffectSpirit

--- src/player.c
+++ src/player.c
@@ -364,7 +364,7 @@ NAKED u32 Player_WeaponEffectAgility(Player* p) { INCFUNC("asm/func/Player_Weapo
 
 u32 Player_WeaponEffectVitality(Player* p) { return p->stats[STAT_VITALITY] >> 3; }
 
-NAKED u32 Player_WeaponEffectSpirit(Player* p) { INCFUNC("asm/func/Player_WeaponEffectSpirit.inc"); }
+u32 Player_WeaponEffectSpirit(Player* p) { return p->stats[STAT_SPIRIT] >> 3; }
 
 NAKED u32 Player_WeaponEffectENE(Player* p) { INCFUNC("asm/func/Player_WeaponEffectENE.inc"); }
 
