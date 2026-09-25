a0415fb06ea66e069c666ba21989793d49034fae
akatsuki105 <bluejapan73+dev@gmail.com>
2026-09-26 05:49:04 +0900

    Entity080ac374_Create を NAKED に戻す (ROM 不一致)

--- src/entity_080ac374.c
+++ src/entity_080ac374.c
@@ -77,15 +77,4 @@ s32 Entity080ac374_Destroy(Entity080ac374* p) {
 
 NAKED s32 Entity080ac374_Init(Entity080ac374* p, Player* player, u8 param_3, Vec3* pos, u16 param_5, u16 param_6) { INCFUNC("asm/func/Entity080ac374_Init.inc"); }
 
-Entity080ac374* Entity080ac374_Create(Player* player, u8 param_2, Vec3* pos, u16 param_4, u16 param_5) {
-  Entity080ac374* p = CreateEntity(ENTITY_UNK_8, sizeof(Entity080ac374));
-
-  if (p != NULL) {
-    SetEntityRoutine(p, Entity080ac374_Update, Entity080ac374_Destroy);
-    if (Entity080ac374_Init(p, player, param_2, pos, param_4, param_5) < 0) {
-      KillEntity((Entity*)p);
-      return NULL;
-    }
-  }
-  return p;
-}
+NAKED Entity080ac374* Entity080ac374_Create(Player* player, u8 param_2, Vec3* pos, u16 param_4, u16 param_5) { INCFUNC("asm/func/Entity080ac374_Create.inc"); }
