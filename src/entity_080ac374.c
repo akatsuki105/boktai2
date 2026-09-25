9e82b2a0b4e6646065bfcb941ef84c037ff276fb
akatsuki105 <bluejapan73+dev@gmail.com>
2026-09-26 02:34:38 +0900

    Entity080ac374

--- src/entity_080ac374.c
+++ src/entity_080ac374.c
@@ -1,4 +1,10 @@
 #include "entity.h"
 #include "global.h"
 
+typedef struct {
+  Entity e;  // ENTITY_UNK_8
+  u8 unk_18[644 - 0x18];
+} Entity080ac374;
+static_assert(sizeof(Entity080ac374) == 644);
+
 INCASM("asm/entity_080ac374.inc");
